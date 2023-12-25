30e60dbbd16ccb19a4eabee142ef6d4b35e9d489
Shane <sotiris@sexycoders.org>
2023-12-25 10:55:08 +0200

    logger works : fetching inverters from db, logging and writing results back

---
+++ main.js
@@ -0,0 +1,62 @@
+const axios = require('axios');
+const { MongoClient } = require('mongodb');
+
+// MongoDB connection URL
+//const url = 'mongo://solar-logger-module-mongo-1:27017';
+const url = 'mongodb://localhost:27019';
+
+// Function to fetch inverter list from MongoDB
+async function fetchInverterList() {
+  process.stdout.write('connecting to mongo on '+url+'...');
+  const client = new MongoClient(url);
+  console.log('DONE!');
+
+  process.stdout.write('fetching inverter list...');
+  try {
+    await client.connect();
+    const db = client.db('info');
+    const collection = db.collection('inverters');
+
+    const inverters = await collection.find({}).toArray();
+    return inverters;
+  } finally {
+    await client.close();
+  }
+  console.log('DONE!');
+}
+
+// Function to process each inverter
+async function processInverter(inverter) {
+  try {
+    const response = await axios.get(inverter.location, { params: { Scope: 'System' }});
+    const inverterData = { id: inverter.id, ...response.data };
+    console.log(inverterData);
+
+    const client = new MongoClient(url);
+    await client.connect();
+    const db = client.db('data');
+    const collection = db.collection('inverter_logs');
+
+    await collection.insertOne(inverterData);
+
+    await client.close();  // Close the connection after insertion
+  } catch (error) {
+    console.error(`Error fetching data for inverter ${inverter.id}: ${error.message}`);
+  }
+}
+
+// Main function to execute the flow
+async function main() {
+  try {
+    const inverters = await fetchInverterList();
+    inverters.forEach(inverter => processInverter(inverter));
+  } catch (error) {
+    console.error('Error in main function:', error.message);
+  }
+}
+
+// Schedule the main function to run every 3 minutes
+setInterval(main, 3 * 60 * 1000);
+
+main();
+
